bdedc69cc94bbab6cde423649836db0019668c0f
yanyanghua <1063676593@qq.com>
2018-09-11 20:59:31 +0800

    init

--- js/api.js
+++ js/api.js
@@ -109,7 +109,27 @@ $(function(){
 	
 		//下载app
 		$('.downBtn').on('click',function(){
-			window.location.href = 'http://android.myapp.com/myapp/detail.htm?apkName=com.rightnoworld.globalview';
+			
+			//window.location.href = 'http://android.myapp.com/myapp/detail.htm?apkName=com.rightnoworld.globalview';
+
+	        //微信内置地图查看位置
+	        wx.ready(function() {
+	            wx.openLocation({
+	                latitude: 117.953314, // 纬度，浮点数，范围为90 ~ -90
+	                longitude: 44.356147, // 经度，浮点数，范围为180 ~ -180。
+	                name: '内蒙古', // 位置名
+	                address: '内蒙古自治区锡林郭勒盟西乌珠穆沁旗', // 地址详情说明
+	                scale: 14, // 地图缩放级别,整形值,范围从1~28。默认为最大
+	                infoUrl: 'http://weixin.qq.com' // 在查看位置界面底部显示的超链接,可点击跳转
+	            });
+	        })
+	    })
+		
+		
 		})
 	
 })
+
+
+
+
